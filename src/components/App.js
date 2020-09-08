d50dd6b8864ddc083e9be1d7a5b83f0e2b6c0214
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-08 18:46:46 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,7 @@ export default function App() {
   // 5- Build a `changeStatus` function that takes an id and
   // changes the `married` from true to false and viceversa
   const changeStatus = id => {
-    
+    // def NOT event handler
   }
 
   // STRETCH - Make a helper function that returns
