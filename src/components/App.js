e55020c0e4f7184d4020562b4b6bdedc4b4412c6
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-08 18:48:24 +0200

    wip

--- src/components/App.js
+++ src/components/App.js
@@ -21,7 +21,7 @@ export default function App() {
   // changes the `married` from true to false and viceversa
   const changeStatus = id => {
     // def NOT event handler
-    setFriends()
+    setFriends(friends.map()/* a new array of friends, made off of the old one */)
   }
 
   // STRETCH - Make a helper function that returns
